eb7a5d551bb720616ce5e3fa5ec0d5f4901c9e04
Murilo Cardoso <65872392+5gMurilo@users.noreply.github.com>
2021-12-29 20:01:53 -0300

    widget adjustment

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+      height: 450,
       child: transactions.isEmpty
           ? Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -69,6 +69,7 @@ class TransactionList extends StatelessWidget {
                         onRemove(transac.id);
                       },
                       color: Theme.of(context).colorScheme.secondary,
+                      hoverColor: Theme.of(context).colorScheme.error,
                     ),
                   ),
                 );
