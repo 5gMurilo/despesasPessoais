8a91d0e9b074abaeffe6745576018bccee95fd72
Murilo Cardoso <65872392+5gMurilo@users.noreply.github.com>
2022-01-03 15:41:03 -0300

    adaptative textfield and adaptative button

---
+++ lib/components/adaptative_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class AdaptativeButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  AdaptativeButton({required this.label, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(label),
+            onPressed: onPressed,
+            color: Theme.of(context).colorScheme.onPrimary,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+          )
+        : TextButton(
+            onPressed: onPressed,
+            child: Text(
+              label,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+            style: TextButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+            ),
+          );
+  }
+}
